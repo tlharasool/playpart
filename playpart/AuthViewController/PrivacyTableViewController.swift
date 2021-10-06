46b710d4d3f9a762dbd871c61884c13bee3989a3
@talharasool700 <talharasool700@gmail.com>
2021-10-06 12:00:35 +0500

    Release Notes. 1- Merge Code

--- playpart/AuthViewController/PrivacyTableViewController.swift
+++ playpart/AuthViewController/PrivacyTableViewController.swift
@@ -9,25 +9,25 @@
 import UIKit
 
 class PrivacyTableViewController: UITableViewController {
-
+    
     @IBOutlet weak var staticTableViewCell1: UITableViewCell!
-            @IBOutlet weak var section2cell1: UITableViewCell!
-            @IBOutlet weak var section2cell2: UITableViewCell!
-          
-          
-            override func viewDidLoad() {
-                super.viewDidLoad()
-                self.tableView.backgroundColor =  .black
-                backgroundColor()
-            }
-            
-            func backgroundColor()
-            {
-                self.staticTableViewCell1.backgroundColor = UIColor.black
-                self.section2cell1.backgroundColor = UIColor.black
-                self.section2cell2.backgroundColor = UIColor.black
-            
-            
-            }
+    @IBOutlet weak var section2cell1: UITableViewCell!
+    @IBOutlet weak var section2cell2: UITableViewCell!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableView.backgroundColor =  .black
+        backgroundColor()
+    }
+    
+    func backgroundColor()
+    {
+        self.staticTableViewCell1.backgroundColor = UIColor.black
+        self.section2cell1.backgroundColor = UIColor.black
+        self.section2cell2.backgroundColor = UIColor.black
+        
+        
+    }
 }
 
