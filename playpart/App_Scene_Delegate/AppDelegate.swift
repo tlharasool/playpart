3d9bc3d79f9e34cc7bb01206358458b153819195
@talharasool700 <talharasool700@gmail.com>
2021-10-13 14:00:57 +0500

    Update Sign  and register

--- playpart/App_Scene_Delegate/AppDelegate.swift
+++ playpart/App_Scene_Delegate/AppDelegate.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 import IQKeyboardManagerSwift
+import Alamofire
+
 //edited
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         IQKeyboardManager.shared.enable = true
+        
+
         return true
     }
 
